615c5c2cc21bab2f04911e2ca30bb4e5e661af6f
Lastor <lastoraya@gmail.com>
2019-10-31 19:25:03 +0800

    [加入] sign up 表單驗證

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const flash = require('connect-flash')
+const session = require('express-session')
 const db = require('./models')
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -8,6 +10,18 @@ const HOST = process.env.HOSTNAME || 'localhost'
 app.engine('hbs', exphbs({ extname: 'hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
+app.use(session({
+  secret: 'forum secret',
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(flash())
+
+app.use((req, res, next) => {
+  res.locals.success = req.flash('success')
+  res.locals.error = req.flash('error')
+  next()
+})
 
 // ==============================
 app.listen(PORT, () => {
--- controllers/userCtrler.js
+++ controllers/userCtrler.js
@@ -1,15 +1,27 @@
 const bcrypt = require('bcryptjs')
 const User = require('../models').User
 
+// custom module
+const { checkSignUp } = require('../lib/checkForm.js')
+
 module.exports = {
   signUpPage: (req, res) => res.render('signUp'),
 
-  signUp: (req, res) => {
+  signUp: async (req, res) => {
     const input = { ...req.body }
+
+    // 檢查表單
+    const error = await checkSignUp(input)
+    if (error.length) return res.render('signup', { input, error })
+
+    // 註冊帳號
     input.password = bcrypt.hashSync(input.password, 10)
 
     User.create(input)
-      .then(user => res.redirect('signup'))
+      .then(user => {
+        req.flash('success', '成功註冊帳號')
+        res.redirect('signin')
+      })
       .catch(err => res.status(422).json(err))
   }
 }
