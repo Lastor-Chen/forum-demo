e78bdf2c48330711affdf561c836c8ff981953ad
Lastor <lastoraya@gmail.com>
2019-11-01 12:40:09 +0800

    [加入] main layout 導覽列

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use(passport.session())
 app.use(flash())
 
 app.use((req, res, next) => {
+  res.locals.user = req.user
   res.locals.success = req.flash('success')
   res.locals.error = req.flash('error')
   next()
