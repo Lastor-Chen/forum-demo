b50c39cda11c71514c4f7bcd418a9f4a243ba3dd
Lastor <lastoraya@gmail.com>
2019-11-01 17:56:03 +0800

    [加入] 套件 method-override

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const exphbs = require('express-handlebars')
 const flash = require('connect-flash')
 const session = require('express-session')
 const passport = require('./config/passport.js')
+const methodOverride = require('method-override')
 const db = require('./models')
 const app = express()
 const PORT = process.env.PORT || 3000
@@ -11,6 +12,7 @@ const HOST = process.env.HOSTNAME || 'localhost'
 app.engine('hbs', exphbs({ extname: 'hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 app.use(session({
   secret: 'forum secret',
   resave: false,
