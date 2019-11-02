573a7942fcc5d5343650141be890954e646b1797
Lastor <lastoraya@gmail.com>
2019-11-02 17:57:08 +0800

    [加入] seeder，引入 faker 套件

---
+++ seeders/20191102090141-seeder.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const bcrypt = require('bcryptjs')
+const faker = require('faker')
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return Promise.all([
+      queryInterface.bulkInsert('Users', [{
+        email: 'root@example.com',
+        password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
+        isAdmin: true,
+        name: 'root'
+      }, {
+        email: 'user1@example.com',
+        password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
+        isAdmin: false,
+        name: 'user1',
+      }, {
+        email: 'user2@example.com',
+        password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
+        isAdmin: false,
+        name: 'user2'
+      }]),
+      queryInterface.bulkInsert('Restaurants',
+        Array.from({ length: 50 }).map(() => ({
+          name: faker.name.findName(),
+          tel: faker.phone.phoneNumber(),
+          address: faker.address.streetAddress(),
+          opening_hours: '08:00',
+          image: faker.image.imageUrl(),
+          description: faker.lorem.text(),
+        }))
+      )
+    ])
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return Promise.all([
+      queryInterface.bulkDelete('Users', null),
+      queryInterface.bulkDelete('Restaurants', null)
+    ])
+  }
+};
