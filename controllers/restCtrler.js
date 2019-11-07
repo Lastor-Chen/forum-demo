3109e2f7cd5253fdbdca3414d798d017bb3f3375
Lastor <lastoraya@gmail.com>
2019-11-07 13:20:58 +0800

    [加入] 前台 restaurants 頁面

--- controllers/restCtrler.js
+++ controllers/restCtrler.js
@@ -1,5 +1,16 @@
+const db = require('../models')
+const Restaurant = db.Restaurant
+const Category = db.Category
+
 module.exports = {
   getRestaurants: (req, res) => {
-    res.render('restaurants')
+    Restaurant.findAll({ include: Category })
+      .then(restaurants => {
+        restaurants.forEach(item => {
+          item.description = item.description.slice(0, 50)
+        })
+        res.render('restaurants', { restaurants })
+      })
+      .catch(err => res.status(422).json(err.toString()))
   }
 }
