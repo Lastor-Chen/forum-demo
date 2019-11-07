b0d1a9be2cbf874c7e7906a53648a86933115756
Lastor <lastoraya@gmail.com>
2019-11-07 13:50:30 +0800

    [修改] 前台 restaurants 頁面，description 字數限制改由前端處理

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.engine('hbs', exphbs({
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use(express.static('public'))
 app.use('/upload', express.static(__dirname + '/upload'))
 app.use(session({
   secret: 'forum secret',
--- controllers/restCtrler.js
+++ controllers/restCtrler.js
@@ -6,10 +6,7 @@ module.exports = {
   getRestaurants: (req, res) => {
     Restaurant.findAll({ include: Category })
       .then(restaurants => {
-        restaurants.forEach(item => {
-          item.description = item.description.slice(0, 50)
-        })
-        res.render('restaurants', { restaurants })
+        res.render('restaurants', { css: 'restaurants', restaurants })
       })
       .catch(err => res.status(422).json(err.toString()))
   }
