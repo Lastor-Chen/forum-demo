4028c5c5de17a4811cbbc80f8760134022a18ef4
Lastor <lastoraya@gmail.com>
2019-11-07 21:40:19 +0800

    [加入] Comment model

---
+++ models/comment.js
@@ -0,0 +1,11 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Comment = sequelize.define('Comment', {
+    text: DataTypes.STRING,
+    UserId: DataTypes.INTEGER,
+    RestaurantId: DataTypes.INTEGER
+  }, {});
+  Comment.associate = function(models) {
+  };
+  return Comment;
+};
