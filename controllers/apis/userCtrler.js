dc392fc81c7a2c11b44163ddb906ed97ffa825ef
Lastor <lastoraya@gmail.com>
2019-11-14 20:59:33 +0800

    [重構] api userCtrler.signIn

--- controllers/apis/userCtrler.js
+++ controllers/apis/userCtrler.js
@@ -1,32 +1,23 @@
 const bcrypt = require('bcryptjs')
 const db = require('../../models')
 const User = db.User
-const { UNAUTHORIZED } = require('http-status-codes')
-
-// JWT
+const { BAD_REQUEST, UNAUTHORIZED } = require('http-status-codes')
 const jwt = require('jsonwebtoken')
-const passportJWT = require('passport-jwt')
-const ExtractJwt = passportJWT.ExtractJwt
-const JwtStrategy = passportJWT.Strategy
 
 module.exports = {
   signIn: async (req, res) => {
     try {
-      const email = req.body.email
-      const password = req.body.password
+      const { email, password } = req.body
 
       // 檢查 input 未填
-      if (!email || !password) return res.json({ status: 'error', message: "required fields didn't exist" })
+      if (!email || !password) return res.status(BAD_REQUEST).json({ status: 'error', message: "required fields didn't exist" })
 
-      // 確認帳號
+      // 確認帳密
       const user = await User.findOne({ where: { email } })
-      if (!user) return res.status(UNAUTHORIZED).json({ status: 'error', message: 'no such user found' })
-
-      // 確認密碼
       const isSuccess = bcrypt.compareSync(password, user.password)
-      if (!isSuccess) return res.status(UNAUTHORIZED).json({ status: 'error', message: 'password did not match' })
+      if (!user || !isSuccess) return res.status(UNAUTHORIZED).json({ status: 'error', message: 'email or password match failed' })
 
-      // pass
+      // 通過
       const payload = { id: user.id }
       const token = jwt.sign(payload, process.env.JWT_SECRET)
       res.json({ status: 'success', message: 'ok', token, user })
