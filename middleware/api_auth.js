feb948b4d08016c8d55b1e0189dbc4ec3d5114f3
Lastor <lastoraya@gmail.com>
2019-11-15 11:13:25 +0800

    [重構] middleware api_auth.js

--- middleware/api_auth.js
+++ middleware/api_auth.js
@@ -4,11 +4,8 @@ module.exports = {
   authenticate: passport.authenticate('jwt', { session: false }),
 
   isAuthedAdmin: (req, res, next) => {
-    if (req.user) {
-      if (req.user.isAdmin) return next()
-      res.json({ status: 'error', message: 'permission denied' })
-    } else {
-      res.json({ status: 'error', message: 'permission denied' })
-    }
+    if (req.user.isAdmin) return next()
+
+    res.json({ status: 'error', message: 'permission denied' })
   }
 }
